1ad293fc624540f5ca0cdffa57df183b77f3377f
warstreetdigital <warstreett@gmail.com>
2026-04-19 07:49:19 +0200

    Add files via upload

---
+++ postcss.config.mjs
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+};
+
+export default config;
